bf0ef6f053941a9043c164a6f493304ff4b309a1
AshuJojo <ashutoshsharma.120599@gmail.com>
2023-08-09 15:45:03 +0530

    Enviornment file usage setup with @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ((process.env.NODE_ENV?.trim() === 'production') ? '.env' : '.env.dev'),
+      isGlobal: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) { this.getEnv() }
+
+  getEnv() {
+    console.log("Env Variable: ", process.env.NODE_ENV);
+    console.log("Config Service: ", this.configService.get<string>('NODE_ENV'));
+    console.log("Using enviornment File: ", (process.env.NODE_ENV?.trim() === 'production') ? '.env' : '.env.dev')
+  }
+}
